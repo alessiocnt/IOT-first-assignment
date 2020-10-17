f3d05185f6d37e714e4bcb14329a7bb24249fc22
Alessio Conti <alessio.conti3@studio.unibo.it>
2020-10-17 17:58:18 +0200

    Leds now follow the fly.

--- sketch/game.cpp
+++ sketch/game.cpp
@@ -3,6 +3,7 @@
 #include "game.h"
  
 void gamingPhase() {
+    digitalWrite(currentPosition, HIGH);
     unsigned long tNow = millis();
     if (tNow - tPression > (k+1)*tMin) {
         state = END_STATE;
